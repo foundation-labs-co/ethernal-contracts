3961a35fb72c3edb6b01fbba0d3cfb696610200f
0xFangHodl <fanghodl@gmail.com>
2023-12-24 16:14:46 +0700

    feat: test send ETH

--- test/ethernalBridge.js
+++ test/ethernalBridge.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { config, tokenIndexes } = require('../config')
+const { config, tokenIndexes, networkId } = require('../config')
+const { deployContract, contractAt, getContractAddress, sendTxn, getFrameSigner } = require('../scripts/lib/deploy')
+
 
 describe("Ethernal Bridge", function () {
     let EthernalBridgeContract
@@ -15,28 +17,48 @@ describe("Ethernal Bridge", function () {
 
     let vaultEthernalEUSDT
 
+    const fee = expandDecimals(1,15); // 0.001 ETH
 
     beforeEach(async function () {
+
+        // Bridge Contract
+        EthernalBridgeContract = await ethers.getContractFactory("EthernalBridge");
+        
+        // XOracle Message Contract
+        MockXOracleMessage = await ethers.getContractFactory("XOracleMessage");
+
+        // Token Contract
         ERC20Token = await ethers.getContractFactory("ERC20Token");
         EthernalToken = await ethers.getContractFactory("EthernalToken");
 
-        EthernalBridgeContract = await ethers.getContractFactory("EthernalBridge");
-
+        // Vault Contract
         VaultMintable = await ethers.getContractFactory("VaultMintable");
         VaultEthernal = await ethers.getContractFactory("VaultEthernal");
+        VaultETH = await ethers.getContractFactory("VaultETH");
+        
+        // Deploy Mock XOracleMessage
+        xOracleMessage = await MockXOracleMessage.deploy();
+        await xOracleMessage.deployed();
 
-        MockXOracleMessage = await ethers.getContractFactory("XOracleMessage");
-
-        // Deploy Token
-        usdt = await ERC20Token.deploy("Ethernal-Peg USDT Token", "USDT");
-        await usdt.deployed();
-
-        eusdt = await EthernalToken.deploy("Ethernal Wrapped Yield USDT", "EUSDT", usdt.address);
-        await eusdt.deployed();
+        await xOracleMessage.setFee(fee);
 
         // Deploy EthernalBridge
-        ethernalBridge = await EthernalBridgeContract.deploy();
+        ethernalBridge = await EthernalBridgeContract.deploy(xOracleMessage.address);
         await ethernalBridge.deployed();
+
+        // Deploy Token
+
+        // USDT - EUSDT
+        usdt = await ERC20Token.deploy("USDT (Ethernal)", "USDT");
+        await usdt.deployed();
+        eusdt = await EthernalToken.deploy("Ethernal Passive Yield USDT", "EUSDT", usdt.address);
+        await eusdt.deployed();
+        
+        // ETH - EETH
+        eth = await ERC20Token.deploy("ETH (Ethernal)", "ETH");
+        await eth.deployed();
+        eeth = await EthernalToken.deploy("Ethernal Passive Yield ETH", "EETH", eth.address);
+        await eeth.deployed();
 
         // Deploy Vault USDT - EUSDT
         vaultMintableUSDT = await VaultMintable.deploy(tokenIndexes.USDT, usdt.address, expandDecimals(1, 18));
@@ -45,14 +67,13 @@ describe("Ethernal Bridge", function () {
         vaultEthernalEUSDT = await VaultEthernal.deploy(tokenIndexes.EUSDT, usdt.address, expandDecimals(1, 18), tokenIndexes.USDT, eusdt.address);
         await vaultEthernalEUSDT.deployed();
 
-        // Deploy Mock XOracleMessage
-        xOracleMessage = await MockXOracleMessage.deploy();
-        await xOracleMessage.deployed();
-        await ethernalBridge.setXOracleMessage(xOracleMessage.address);
+        // Deploy Vault ETH 
+        vaultETH = await VaultETH.deploy(tokenIndexes.ETH, eth.address, expandDecimals(1, 18), "0x0000000000000000000000000000000000000000");
+        await vaultETH.deployed();
+
     });
 
     it("Only owner can call this function", async function () {
-
         const [deployer, account2, account3] = await ethers.getSigners();
         const {AddressZero} = ethers.constants
 
@@ -76,12 +97,6 @@ describe("Ethernal Bridge", function () {
         await expect(ethernalBridge.connect(deployer).addAllowToken(AddressZero, vaultMintableUSDT.address))
         .to.be.revertedWith("invalid address");
 
-        // const chainId = await vaultMintableUSDT.chainId();
-        // console.log("chainId: ", chainId.toString());
-        
-        // const tokenIndex = await vaultMintableUSDT.tokenIndex();
-        // console.log("tokenIndex: ", tokenIndex.toString());
-        
         await ethernalBridge.connect(deployer).addAllowToken(usdt.address, vaultMintableUSDT.address);
 
         await expect(ethernalBridge.connect(deployer).addAllowToken(usdt.address, vaultMintableUSDT.address))
@@ -121,12 +136,20 @@ describe("Ethernal Bridge", function () {
 
     });
 
-    it("send", async function () {
+    it("Set Support Dst Token Index", async function () {
+        const [deployer, account2, account3] = await ethers.getSigners();
+        const {AddressZero} = ethers.constants
+        const chainIdHardHat = await ethernalBridge.chainId();
+
+        await expect(ethernalBridge.connect(deployer).setSupportDstTokenIndex(chainIdHardHat, tokenIndexes.USDT, true))
+        .to.be.revertedWith("invalid chainId");
+    });
+
+    it("Send", async function () {
         const [deployer, account2, account3, endpoint] = await ethers.getSigners();
         const {AddressZero} = ethers.constants
 
-        const chainIdHardHat = await vaultMintableUSDT.chainId();
-        console.log("chainId: ", chainIdHardHat.toString());
+        const chainIdHardHat = await ethernalBridge.chainId();
         const chainIdBSC = 56;
 
         // Bridge EUSDT -> USDT  
@@ -134,48 +157,104 @@ describe("Ethernal Bridge", function () {
         // vault vaultEthernal -> vaultVenus
 
         // send token address = AddressZero
-        await expect(ethernalBridge.connect(account2).send(AddressZero, 0, chainIdHardHat, tokenIndexes.USDT, account2.address))
+        await expect(ethernalBridge.connect(account2).send(AddressZero, 0, chainIdHardHat, tokenIndexes.USDT, account2.address, {value: 0}))
         .to.be.revertedWith("invalid token address");
 
         // srcChainId = dstChainId
-        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdHardHat, tokenIndexes.USDT, AddressZero))
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdHardHat, tokenIndexes.USDT, AddressZero, {value: 0}))
         .to.be.revertedWith("invalid chainId");
 
         // receiver = AddressZero
-        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, 0, tokenIndexes.USDT, AddressZero))
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, 0, tokenIndexes.USDT, AddressZero, {value: 0}))
         .to.be.revertedWith("invalid receiver address");
 
+        // check fee
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, 0, tokenIndexes.USDT, account2.address, {value: 0}))
+        .to.be.revertedWith("insufficient fee");
+
         // not yet set endpoint
-        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, 0, tokenIndexes.USDT, account2.address))
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, 0, tokenIndexes.USDT, account2.address, {value: fee}))
         .to.be.revertedWith("invalid endpoint address");
 
         await ethernalBridge.connect(deployer).setEndpoint(chainIdBSC, endpoint.address)
 
         // not yet add allow token
-        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdBSC, tokenIndexes.USDT, account2.address))
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdBSC, tokenIndexes.USDT, account2.address, {value: fee}))
         .to.be.revertedWith("token not allowed");
 
         await ethernalBridge.connect(deployer).addAllowToken(eusdt.address, vaultEthernalEUSDT.address);
         
-        // vault not set support Token
-        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdBSC, tokenIndexes.USDT, account2.address))
-        .to.be.revertedWith("tokenIndex not allowed");
+        // bridge not set support dst token
+        await expect(ethernalBridge.connect(account2).send(eusdt.address, 0, chainIdBSC, tokenIndexes.USDT, account2.address, {value: fee}))
+        .to.be.revertedWith("dstTokenIndex not allowed");
 
-        await vaultEthernalEUSDT.connect(deployer).setSupportTokenIndex(tokenIndexes.USDT, true);
+        await ethernalBridge.connect(deployer).setSupportDstTokenIndex(chainIdBSC, tokenIndexes.USDT, true);
 
+        // set controller for mint usdt
         await usdt.connect(deployer).setController(deployer.address, true);
-        await usdt.connect(deployer).setController(vaultEthernalEUSDT.address, true);
         await usdt.connect(deployer).mint(account2.address, expandDecimals(100, 18));
         await usdt.connect(account2).approve(eusdt.address, expandDecimals(100, 18));
+
         // deposit usdt get 100 eusdt
         await eusdt.connect(account2).deposit(expandDecimals(100, 18));
-        
+
+        // set controller for burn
+        await usdt.connect(deployer).setController(vaultEthernalEUSDT.address, true);
+
         // approve eusdt to ethernalBridge
         await eusdt.connect(account2).approve(ethernalBridge.address, expandDecimals(100, 18));
 
         await vaultEthernalEUSDT.connect(deployer).setController(ethernalBridge.address);
 
-        await ethernalBridge.connect(account2).send(eusdt.address, expandDecimals(100, 18), chainIdBSC, tokenIndexes.USDT, account2.address);
+        await ethernalBridge.connect(account2).send(eusdt.address, expandDecimals(100, 18), chainIdBSC, tokenIndexes.USDT, account2.address, {value: fee});
+    });
+
+    it("Send ETH", async function () {
+        const [deployer, account2, account3, endpoint] = await ethers.getSigners();
+        const {AddressZero} = ethers.constants
+
+        const chainIdHardHat = await vaultETH.chainId();
+        const chainIdBSC = 56;
+        const ETH_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+        const amount = 1; // 1 ETH
+
+        // Bridge ETH -> ETH  
+        // Chain hardhat -> BSC
+        // vault vaultETH -> vaultVenus
+
+        // srcChainId = dstChainId
+        await expect(ethernalBridge.connect(account2).sendETH(chainIdHardHat, tokenIndexes.ETH, AddressZero, {value: 0}))
+        .to.be.revertedWith("invalid chainId");
+
+        // receiver = AddressZero
+        await expect(ethernalBridge.connect(account2).sendETH(0, tokenIndexes.ETH, AddressZero, {value: 0}))
+        .to.be.revertedWith("invalid receiver address");
+
+        // check fee
+        await expect(ethernalBridge.connect(account2).sendETH(0, tokenIndexes.ETH, account2.address, {value: 0}))
+        .to.be.revertedWith("insufficient fee");
+
+        // not yet set endpoint
+        await expect(ethernalBridge.connect(account2).sendETH(0, tokenIndexes.ETH, account2.address, {value: fee + amount}))
+        .to.be.revertedWith("invalid endpoint address");
+
+        await ethernalBridge.connect(deployer).setEndpoint(chainIdBSC, endpoint.address)
+
+        // not yet add allow token
+        await expect(ethernalBridge.connect(account2).sendETH(chainIdBSC, tokenIndexes.ETH, account2.address, {value: fee + amount}))
+        .to.be.revertedWith("token not allowed");
+
+        await ethernalBridge.connect(deployer).addAllowToken(ETH_TOKEN, vaultETH.address);
+
+        // bridge not set support Token
+        await expect(ethernalBridge.connect(account2).sendETH(chainIdBSC, tokenIndexes.ETH, account2.address, {value: fee + amount}))
+        .to.be.revertedWith("dstTokenIndex not allowed");
+
+        await ethernalBridge.connect(deployer).setSupportDstTokenIndex(chainIdBSC, tokenIndexes.ETH, true);
+
+        await vaultETH.connect(deployer).setController(ethernalBridge.address);
+
+        await ethernalBridge.connect(account2).sendETH(chainIdBSC, tokenIndexes.ETH, account2.address, {value: fee + amount});
     });
 })
 
