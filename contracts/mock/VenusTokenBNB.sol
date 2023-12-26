caf3068d21bab62f62eea2d901b5af6371fb94dc
0xFangHodl <fanghodl@gmail.com>
2023-12-26 15:46:52 +0700

    feat: add mock venus token bnb contract

---
+++ contracts/mock/VenusTokenBNB.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract VenusTokenBNB {
+
+    receive() external payable {}
+
+    function mint() external payable returns(uint) {
+    
+        return 0;
+    }
+    function redeemUnderlying(uint redeemAmount) external returns (uint) {
+        payable(msg.sender).transfer(redeemAmount);
+        return 0;
+    }
+}
