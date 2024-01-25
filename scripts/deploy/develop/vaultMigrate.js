d097c73cb4510d030df0f664329cea953d89a166
0xFangHodl <fanghodl@gmail.com>
2024-01-25 11:20:10 +0700

    feat: add vaultMigrate script

---
+++ scripts/deploy/develop/vaultMigrate.js
@@ -0,0 +1,96 @@
+const { deployContract, contractAt, getContractAddress, sendTxn, getFrameSigner } = require('../../lib/deploy')
+const { networkId, config, tokenIndexes } = require('../../../config')
+
+async function main() {
+  let deployer = await getFrameSigner()
+
+  const srcChain = networkId.develop
+
+  // get EthernalBridge deployed
+  const ethernalBridge = await contractAt('EthernalBridge', getContractAddress('ethernalBridge'), deployer)
+
+  // migrate
+  for (let i = 0; i < config.chains[srcChain].vaultTokens.length; i++) {
+    const vaultToken = config.chains[srcChain].vaultTokens[i]
+    if (vaultToken.type == 'VaultEthernal') {
+      // remove controller previous vault
+      const token = await contractAt('ERC20Token', getContractAddress(vaultToken.tokenName), deployer)
+      const prevVaultAddress = getContractAddress(`vault${vaultToken.name}`)
+      await sendTxn(token.setController(prevVaultAddress, false), `${vaultToken.tokenName}.setController(${prevVaultAddress}, false)`)
+
+      // deploy new VaultEthernal
+      const vault = await deployContract(
+        vaultToken.type,
+        [
+          vaultToken.tokenIndex,
+          getContractAddress(vaultToken.tokenName),
+          vaultToken.minDeposit,
+          getContractAddress(vaultToken.ethernalTokenName),
+        ],
+        `Vault${vaultToken.name}`,
+        deployer
+      )
+
+      const ethernalTokenAddress = getContractAddress(vaultToken.ethernalTokenName)
+
+      // set controller
+      await sendTxn(vault.setController(ethernalBridge.address), `vault.setController(${ethernalBridge.address})`)
+      await sendTxn(token.setController(vault.address, true), `${vaultToken.tokenName}.setController(${vault.address}, true)`)
+
+      // removeAllowToken
+      const isExist = await ethernalBridge.tokenVaults(ethernalTokenAddress)
+      if (isExist != '0x0000000000000000000000000000000000000000') {
+        // removeAllowToken
+        await sendTxn(ethernalBridge.removeAllowToken(ethernalTokenAddress), `ethernalBridge.removeAllowToken(${ethernalTokenAddress})`)
+      }
+
+      // addAllowTokens
+      await sendTxn(
+        ethernalBridge.addAllowToken(ethernalTokenAddress, vault.address),
+        `ethernalBridge.addAllowToken(${ethernalTokenAddress}, ${vault.address})`
+      )
+    }
+    else if (vaultToken.type == 'VaultMintable') {
+      // remove controller previous vault
+      const token = await contractAt('ERC20Token', getContractAddress(vaultToken.tokenName), deployer)
+      const prevVaultAddress = getContractAddress(`vault${vaultToken.name}`)
+      await sendTxn(token.setController(prevVaultAddress, false), `${vaultToken.tokenName}.setController(${prevVaultAddress}, false)`)
+
+      // deploy new VaultMintable
+      const vault = await deployContract(
+        vaultToken.type,
+        [
+          vaultToken.tokenIndex,
+          getContractAddress(vaultToken.tokenName),
+          vaultToken.minDeposit,
+        ],
+        `Vault${vaultToken.name}`,
+        deployer
+      )
+
+      // set controller
+      await sendTxn(vault.setController(ethernalBridge.address), `vault.setController(${ethernalBridge.address})`)
+      await sendTxn(token.setController(vault.address, true), `${vaultToken.tokenName}.setController(${vault.address}, true)`)
+
+      // removeAllowToken
+      const isExist = await ethernalBridge.tokenVaults(token.address)
+      if (isExist != '0x0000000000000000000000000000000000000000') {
+        // removeAllowToken
+        await sendTxn(ethernalBridge.removeAllowToken(token.address), `ethernalBridge.removeAllowToken(${token.address})`)
+      }
+
+      // addAllowTokens
+      await sendTxn(
+        ethernalBridge.addAllowToken(token.address, vault.address),
+        `ethernalBridge.addAllowToken(${token.address}, ${vault.address})`
+      )
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
